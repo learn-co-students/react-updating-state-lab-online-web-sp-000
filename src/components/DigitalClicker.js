f9819286275cd05f8e56545da897314666062f14
github username <github email address>
2019-10-17 16:13:55 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
-// Code DigitalClicker Component Here
+// Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  increment = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.increment}>{this.state.timesClicked}</button>
+    )
+  }
+}
