b265396fcffa0cbd0441f66ebcf5df08f4150b34
ch2isk4kos <ch2isk4kos@gmail.com>
2020-01-26 17:48:12 -0500

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,66 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        };
+    }
+
+    handleBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    handleResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.video,
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <label>Bitrate</label>
+                <button className='bitrate' onClick={this.handleBitrate}>{this.state.settings.bitrate}</button>
+                <label>Resolution</label>
+                <button className='resolution' onClick={this.handleResolution}>{this.state.settings.video.resolution}</button>
+            </div>
+        )
+    }
+
+}
+
+// const updatedHouse = deepMerge(house, {
+//     kitchen: {
+//         table: {
+//             legs: 8
+//         }
+//     }
+// });
+
+// this.setState({
+//     settings: {
+//         bitrate: 12
+//     }
+// })
