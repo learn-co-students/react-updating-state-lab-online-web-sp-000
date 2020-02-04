a00222615811b0721db90682a8652162758862c2
Fabi <fafi@Fabis-MBP.attlocal.net>
2020-02-03 22:32:19 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,25 +1,51 @@
 // Code DigitalClicker Component Here
-import React from 'react'
+// import React from 'react'
 
-class DigitalClicker extends React.Components {
-    constructor() {
-    super() 
+// class DigitalClicker extends React.Component {
+//     constructor() {
+//     super() 
 
-   this.state = {
-        timesClicked: 0,
-    }
+//    this.state = {
+//         timesClicked: 0,
+//     }
+// }
+
+//     handleClick = () => {
+//         this.setState(prevState => ({
+//             thisClicked: prevState.timesClicked+1
+//         }))
+// }
+
+// render() {
+//     return(
+//         <button onClick={this.handleClick}> {this.state.timesClicked}</button>
+//     )
+// }
+// }
+// export default DigitalClicker
+
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      timesClicked: prevState.timesClicked+1
+    }))
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    );
+  }
 }
 
-    handleClick = () => {
-        this.setState(prevState => ({
-            thisClicked: prevState.timesClicked+1
-        }))
-}
-
-render() {
-    return(
-        <button onClick={this.handleClick}> {this.state.timesClicked}</button>
-    )
-}
-}
 export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,56 +1,111 @@
 // Code YouTubeDebugger Component Here
-import React from 'react'
+// import React from 'react'
 
-class YouTubeDebugger extends React.Component{
-    constructor () {
-        super()
+// class YouTubeDebugger extends React.Component{
+//     constructor () {
+//         super()
 
-        this.state = {
-            errors:[],
-            user: null,
-            setting: {
-                bitrate: 8,
-                video: {
-                    resolution: '1080p'
-                }
-            }
-          }
+//         this.state = {
+//             errors:[],
+//             user: null,
+//             settings: {
+//                 bitrate: 8,
+//                 video: {
+//                     resolution: "1080p"
+//                 }
+//             }
+//           }
+//         }
+    
+//     handleChangeBitrate = () => {
+//         this.setState({
+//             settings: {
+//                 ...this.state.setting,
+//                 bitrate: 12
+//             }
+//         })
+//     }
+
+//     handleChangeResolution = () => {
+//         this.setState({
+//             settings: {
+//                 ...this.state.settings,
+//                 video: {
+//                     ...this.state.settings.video,
+//                     resolution:"720p"
+//                 }
+//             }
+//         });
+    
+//     };
+
+//     render() {
+//         return(
+//             <div>
+//                 <button className="bitrate" onClick={this.handleChangeBitrate}>
+//                     Change bitrate
+//                 </button>
+//                 <button className="resolution"> onClick={this.handleChangeResolution}>
+//                     Change resolution
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
+
+// export default YouTubeDebugger
+
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
         }
-    
-    handleChangeBitrate = () => {
-        this.setState({
-            settings: {
-                ...this.state.setting,
-                bitrate: 12
-            }
-        })
-    }
-
-    handleChangeResolution = () => {
-        this.setState({
-            settings: {
-                ...this.state.settings,
-                video: {
-                    ...this.state.settings.video,
-                    resolution: '720'
-                }
-            }
-        });
-    
+      }
     };
+  }
 
-    render() {
-        return(
-            <div>
-                <button className="bitrate" onClick={this.handleChangeBitrate}>
-                    Change bitrate
-                </button>
-                <button className="resolution"> onClick={this.handleChangeResolution}>
-                    Change resolution
-                </button>
-            </div>
-        )
-    }
+  handleChangeBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  };
+
+  handleChangeResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleChangeBitrate}>
+          Change bitrate
+        </button>
+        <button className="resolution" onClick={this.handleChangeResolution}>
+          Change resolution
+        </button>
+      </div>
+    );
+  }
 }
 
-export default YouTubeDebugger
+export default YouTubeDebugger;
