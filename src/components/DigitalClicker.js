6ec55c22e5a5ed4699fd658f424504adb0efb614
oliverpdahl <oliverpdahl@gmail.com>
2020-04-19 16:23:45 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  increment = () => {
+    const newClicks = this.state.timesClicked + 1
+    this.setState(
+      {timesClicked: newClicks}
+    )
+  }
+  render() {
+    return (
+      <button onClick={this.increment}>{this.state.timesClicked}</button>
+    )
+  }
+}
