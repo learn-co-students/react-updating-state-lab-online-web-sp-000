ddb301215a63f93b0841956ef40d530797b0aeab
Lauren McCoy <lm_lutec@outlook.com>
2020-07-01 09:53:30 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends React.Component {
+    constructor(){
+        super()
+
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    updateTimesClicked = () => {
+        this.setState(previousState => {
+            return {
+             timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.updateTimesClicked}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,48 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+import { render } from 'enzyme';
+
+export default class YouTubeDebugger extends React.Component {
+    constructor(){
+        super()
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    changeBitrateEvent = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    changeResolutionEvent = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings, 
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.changeBitrateEvent} className="bitrate">Bitrate</button>
+                <button onClick={this.changeResolutionEvent} className="resolution">Resolution</button>
+            </div>
+        )
+    }
+}
