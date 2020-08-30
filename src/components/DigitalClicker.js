9cb265423bbff2a5f4faa8a86d07fcda1235e18a
Nisticism <nistacular@gmail.com>
2020-08-29 17:27:19 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = (event) => {
+        let time = this.state.timesClicked
+        this.setState({timesClicked: time + 1})
+    }
+
+    render() {
+        return (
+            <button
+            onClick={this.handleClick}>
+                {this.state.timesClicked}
+            </button>   
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,60 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    handleBitrate = (event) => {
+        this.setState({
+            errors: [],
+            user: null,
+            settings: {
+                ...this.state.settings, 
+                bitrate: 12
+            }
+        })
+    }
+
+    handleResolution = (event) => {
+        this.setState({
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+            video: {
+                ...this.state.settings.video, 
+                resolution: '720p'
+            }  
+        }      
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.handleBitrate}>
+
+                </button>
+
+                <button className="resolution" onClick={this.handleResolution}>
+
+                </button>
+
+            </div>
+        )
+    }
+}
