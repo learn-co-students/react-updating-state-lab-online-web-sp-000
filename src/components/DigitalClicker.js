8c9be89b2752be67bfc50f0900449ebe88ac7be8
Anthonyntilelli <Anthony@tilelli.me>
2020-09-11 07:35:35 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,13 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            timesClicked: 0
+        };
+    }
+    clickHandler = () => this.setState((prevState) => {timesClicked: prevState.timesClicked+1})
+    render = () => <button onClick={this.clickHandler}>{this.state.timesClicked}</button>
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,33 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+   constructor(){
+       super();
+       this.state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+      }
+   }
+
+   bitrateHandler = () => this.setState({ settings: { bitrate: 12 }});
+   resolutionHandler = () => this.setState({ settings: { resolution: '720p'}});
+
+   render = () => {
+       return(
+           <>
+             <button className="bitrate" onClick={this.bitrateHandler}></button>
+             <button className="resolution" onClick={this.resolutionHandler}></button>
+           </>
+       );
+   }
+
+
+
+}1
