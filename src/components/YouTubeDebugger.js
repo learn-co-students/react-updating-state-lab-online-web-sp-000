2a5f1b8ae01f32bc54533a37c775790109958511
connorworthen <connorw220@protonmail.com>
2020-11-16 17:06:42 -0800

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,28 @@
+const { Component } = require("react");
+
 // Code YouTubeDebugger Component Here
+export default class YouTubeDebugger extends Component {
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+      bitrate: 8,
+      video: {
+      resolution: '1080p'
+    }
+  }
+    }
+  }
+
+  handleClick = () => {
+    this.
+  }
+
+  render(){
+    return(
+      <button class="bitrate" onClick={this.handleClick}></button>
+    )
+  }
+}
