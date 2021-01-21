b1372997fcb44e4fafd2c3d6499e0a8e7ee774b8
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-21 11:37:51 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+	constructor() {
+		super()
+		this.state = {
+			timesClicked: 0
+		}
+	}
+
+	increment = () => {
+		this.setState(previousState => {
+			return {timesClicked: previousState.timesClicked += 1}	
+		})
+	}	
+
+	render() {
+		return (<button onClick={this.increment}>{this.state.timesClicked}</button>)
+	}
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+class YouTubeDebugger extends Component {
+	constructor() {
+		super();
+		this.state = {
+	    errors: [],
+	    user: null,
+	    settings: {
+	      bitrate: 8,
+	      video: {
+	        resolution: '1080p'
+	      }
+	    }
+	  };
+	};
+
+	bitrateChange = () => {
+		this.setState(previousState => {
+			return {settings: { 
+				...previousState.settings,
+				bitrate: previousState.settings.bitrate === 8 ? 12 : 8
+			}};
+		})
+	};
+
+	resolutionChange = () => {
+		this.setState(previousState => {
+			return {settings: { ...previousState.settings,
+				video: {
+					...previousState.settings.video,
+					resolution: previousState.settings.video.resolution === '1080p' ? '720p' : '1080p' }
+			}}
+		})
+	};
+
+	render() {
+		return (
+			<div>
+				<button className='bitrate' onClick={this.bitrateChange}> 
+					Bitrate {this.state.settings.bitrate} 
+				</button>
+				<button className='resolution' onClick={this.resolutionChange}> 
+					Resolution {this.state.settings.video.resolution} 
+				</button>
+			</div>
+		)
+	};
+}
+
+export default YouTubeDebugger;
