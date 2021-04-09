f20be5949c8f52c39fed6a5f8f26f5465cd575a6
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-04-08 18:17:31 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import DigitalClicker from './components/DigitalClicker';
-import YouTubeDebugger from './components/YouTubeDebugger';
+// import YouTubeDebugger from './components/YouTubeDebugger';
 
 ReactDOM.render(
   <div>
     <DigitalClicker />
-    <YouTubeDebugger />
   </div>,
   document.getElementById('global')
 );
